0921caff76ef0493863e954b7789869d4594bb7e
AndrewPlum <144956746+AndrewPlum@users.noreply.github.com>
2024-02-28 21:10:54 -0800

    Create GameManager.cs

---
+++ MidSliceMad/Assets/AndAssets/GameManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+   public static GameManager Instance;
+
+   private int score = 0;
+   public Text scoreText;
+
+   private void Awake()
+   {
+      if (Instance == null)
+         Instance = this;
+      else
+         Destroy(gameObject);
+   }
+
+   private void Start()
+   {
+      UpdateScoreText();
+   }
+
+   public void IncreaseScore()
+   {
+      score++;
+      UpdateScoreText();
+   }
+
+   private void UpdateScoreText()
+   {
+      scoreText.text = "Score: " + score.ToString();
+   }
+}
