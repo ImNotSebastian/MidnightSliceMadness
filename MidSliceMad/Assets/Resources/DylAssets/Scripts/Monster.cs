871c0712b6f86bc8aedaf1f09329afa7ebba6d72
Dylan <thom1892@@vandals.uidaho.edu>
2024-03-20 18:08:43 -0700

    Finished ghost attack

--- MidSliceMad/Assets/Resources/DylAssets/Scripts/Monster.cs
+++ MidSliceMad/Assets/Resources/DylAssets/Scripts/Monster.cs
@@ -8,8 +8,10 @@ public abstract class Monster : MonoBehaviour
     [SerializeField] private float speed = 4f;
     [SerializeField] private float attackDamage = 10f;
     [SerializeField] private float detectionRadius = 9f; // Default detection radius
-    [SerializeField] private float bounceForce = 200f;
+    [SerializeField] private float bounceForce = 1f;
+    [SerializeField] private float bounceCooldown = 1f; // Time in seconds before pursuing again
 
+    private bool isBouncing = false; // Flag to track bouncing state
     protected Transform playerTransform;
     private Rigidbody2D rb;
 
@@ -25,7 +27,7 @@ public abstract class Monster : MonoBehaviour
 
     protected void PursuePlayer()
     {
-        if (Vector3.Distance(transform.position, playerTransform.position) <= detectionRadius)
+        if (!isBouncing && Vector3.Distance(transform.position, playerTransform.position) <= detectionRadius)
         {
             Vector3 direction = (playerTransform.position - transform.position).normalized;
             transform.position += direction * speed * Time.deltaTime;
@@ -44,9 +46,21 @@ public abstract class Monster : MonoBehaviour
             Vector2 bounceDirection = transform.position - collision.transform.position;
             bounceDirection.Normalize(); // Ensure the direction vector has a magnitude of 1
 
-            Debug.Log($"Bounce direction-Bounce force: {bounceDirection} - {bounceForce}");
+            // Begin bounce cooldown period
+            isBouncing = true;
+            Invoke(nameof(ResetBounceState), bounceCooldown); // Schedule reset of bounce state
+
+            rb.velocity = Vector2.zero; // Resets the velocity
+
+            // Debug.Log($"Bounce direction-Bounce force: {bounceDirection} - {bounceForce}");
             // Apply a force to bounce off
             rb.AddForce(bounceDirection * bounceForce, ForceMode2D.Impulse);
         }
+    }
+
+    // Reset the bounce state after cooldown
+    private void ResetBounceState()
+    {
+        isBouncing = false;
     }
 }
