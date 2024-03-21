061e2482ee0a6425da4229f5acfe9c60b918961c
Zoe Abbott <aliecbot@gmail.com>
2024-03-20 20:56:02 -0700

    Prototype test, a little broken, no compiler errors

---
+++ MidSliceMad/Assets/Resources/ZoeAssets/Tests/PlayModeZoe/HUDNumberTest.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.UI;
+using TMPro;
+
+public class HUDNumberTest
+{
+    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+    // `yield return null;` to skip a frame.
+    [UnityTest]
+    public IEnumerator HUDNumberTestWithEnumeratorPasses()
+    {
+        GameObject GM = GameObject.Find("GameManager");
+        GM.GetComponent<DistanceDisplay>().distance += 1;
+        // Use yield to skip a frame.
+        yield return null;
+    }
+}
