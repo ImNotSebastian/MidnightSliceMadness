7e995125f816e208a9a71e2377309e12cac5888b
Dylan <thom1892@@vandals.uidaho.edu>
2024-04-06 23:06:57 -0700

    Worked on main menu

---
+++ MidSliceMad/Assets/Resources/DylAssets/Scripts/GameOverMenu.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverMenu : MonoBehaviour
+{
+    public void LoadMainGame()
+    {
+        SceneManager.LoadScene("MainLevel");
+    }
+
+    public void LoadTutorial()
+    {
+        SceneManager.LoadScene("TutorialLevel");
+    }
+}
