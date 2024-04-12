f83051ab9aec2e60bbfce435efbd617481f94be9
AlisonCasket <aliecbot@gmail.com>
2024-04-12 02:29:01 -0700

    Adding progress Quest/Observer

--- MidSliceMad/Assets/Resources/ZoeAssets/Scripts/Observer.cs
+++ MidSliceMad/Assets/Resources/ZoeAssets/Scripts/Observer.cs
@@ -1,10 +1,42 @@
+/*
+Name: Zoe Abbott
+Role: Team Lead 2: Software Architect 
+Project: Midnight Slice Madness
+
+The Observer. This is using the observer pattern
+to notify the HUD and QuestManager to allow 
+HUD to update to the current quest information.
+*/
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Observer 
 {
-    private GameObject destination;
-    private enum status {Incomplete, InProgress, Complete}
     //will get this information from quest and give to HUD elements quest/destination.
+
+    //Data section: The information passed through by the quest's notification.
+    private TMP_Text questIntro;
+    private GameObject destination;
+    private string questName;
+    private int questProgress;
+    //Data Section Over.
+
+    //Most observers would notify all subscribed subjects,
+    //mine will notify as if it were two different observers
+
+    //Observe HUD functions
+    void NotifyHUD ()
+    {
+
+    }
+
+    //Observe quest functions
+    void NotifyQuest()
+    {
+
+    }
+
 }
--- MidSliceMad/Assets/Resources/ZoeAssets/Scripts/Quest.cs
+++ MidSliceMad/Assets/Resources/ZoeAssets/Scripts/Quest.cs
@@ -1,5 +1,14 @@
+/*
+Name: Zoe Abbott
+Role: Team Lead 2: Software Architect 
+Project: Midnight Slice Madness
+
+{description}
+*/
+
 using System.Collections;
 using System.Collections.Generic;
+using Codice.CM.Common;
 using TMPro;
 using UnityEditor;
 using UnityEngine;
@@ -17,5 +26,45 @@ public class Quest
         //observer will give destination and status to HUD which are on the notif list. 
     }
 
+    void ChangeQuestProgress()
+    {
+        switch(questProgress)
+        {
+            case 0:
+                questProgress += 1;
+                SetQuestProgress1();
+                break;
+            case 1:
+                questProgress += 2;
+                SetQuestProgressComplete();
+                break;
+            case 2:
+                break;
+        }
+    }
+    protected void SetQuestProgress1()
+    {
+        questMessage.text = "Deliver the pizza to {insertlocation}";
+        //destination = GameObject.Find("NextLocation");
+        notifyObserver();
+    }
+
+    protected void SetQuestProgressComplete()
+    {
+        questMessage.text = "Quest Complete!";
+        notifyObserver();
+    }
+
+        protected virtual void SetQuestAttributes()
+    {
+        questMessage.text = "GET THAT PIZZA SIS!!! YAAASSSS!!!";
+        destination = GameObject.Find("OutPizza");
+        questName = "QUEST???";
+        questProgress = 0;
+        notifyObserver();
+    }
+
+
+    
 
 }
