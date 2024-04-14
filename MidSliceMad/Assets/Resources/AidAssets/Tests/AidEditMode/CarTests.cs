f26f150a7042d4ab3300e7d36888c70a13807507
Aiden Shepard <shep8121@vandals.uidaho.edu>
2024-04-14 12:07:12 -0700

    Updated edit/play mode test -AS

---
+++ MidSliceMad/Assets/Resources/AidAssets/Tests/AidEditMode/CarTests.cs
@@ -0,0 +1,39 @@
+/*
+Name: Aiden Shepard
+Role: Team Lead 3 -- QA Manager
+Project: Midnight Slice Madness
+
+This file is composed of the edit mode test for the BicycleController.cs file
+*/
+
+using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+public class CarTests
+{
+    private VehicleController vehicleController;
+
+    [SetUp]
+    public void SetUp()
+    {
+        // Create a GameObject with BicycleController attached
+        GameObject gameObject = new GameObject();
+        vehicleController = gameObject.AddComponent<VehicleController>();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        // Destroy the GameObject and clean up
+        GameObject.DestroyImmediate(vehicleController.gameObject);
+    }
+
+    [Test]
+    public void TestSetMaxSpeed()
+    {
+        // Set and get max speed
+        vehicleController.SetMaxSpeed(10f);
+        Assert.AreEqual(10f, vehicleController.GetMaxSpeed());
+    }
+}
