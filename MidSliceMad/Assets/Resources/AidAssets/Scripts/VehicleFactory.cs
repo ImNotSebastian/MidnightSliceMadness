623aef405167536dc9d1241ef5b71bd929a66d63
Aiden Shepard <shep8121@vandals.uidaho.edu>
2024-04-14 14:41:13 -0700

    Implemented factory pattern -AS

---
+++ MidSliceMad/Assets/Resources/AidAssets/Scripts/VehicleFactory.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class VehicleFactory : MonoBehaviour
+{
+    [SerializeField] private GameObject[] vehiclePrefabs;
+
+    private GameObject currentVehicle;
+
+    void Awake()
+    {
+        Transform parent = transform.parent;
+
+        // Initial creation of the first vehicle at the origin with no rotation
+        currentVehicle = Instantiate(vehiclePrefabs[0], parent);
+    }
+
+    // Example method to demonstrate usage
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) // Check if the user pressed 1
+        {
+            CreateVehicle(0); // Bicycle
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2)) // Check if the user pressed 2
+        {
+            CreateVehicle(1); // Motorcycle
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3)) // Check if the user pressed 3
+        {
+            CreateVehicle(2); // Car
+        }
+    }
+
+    // Method to create vehicle based on user input
+    public void CreateVehicle(int userInput)
+    {
+        if (userInput >= 0 && userInput < vehiclePrefabs.Length) // Checking if userInput is within bounds
+        {
+            Transform parent = transform.parent;
+
+            // Instantiating the selected prefab with the position and rotation of the previous vehicle
+            GameObject newVehicle = Instantiate(vehiclePrefabs[userInput], currentVehicle.transform.position, currentVehicle.transform.rotation, parent);
+
+            // Removing the previous vehicle
+            Destroy(currentVehicle);
+
+            // Updating the reference to the current vehicle
+            currentVehicle = newVehicle;
+        }
+        else
+        {
+            Debug.LogError("Invalid input! Please input a number between 1 and " + vehiclePrefabs.Length);
+        }
+    }
+}
